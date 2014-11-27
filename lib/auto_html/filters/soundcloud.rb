fb8595acc19021e6d4c4f3374bc9e1ad4ea00834
Sam <sam@bigcurl.de>
2014-11-27 17:02:49 +0100

    added new regex for soundcloud

--- lib/auto_html/filters/soundcloud.rb
+++ lib/auto_html/filters/soundcloud.rb
@@ -6,7 +6,7 @@ require 'uri'
 AutoHtml.add_filter(:soundcloud).with(:width => '100%', :height => 166, :auto_play => false, :theme_color => '00FF00', :color => '915f33', :show_comments => false, :show_artwork => false) do |text, options|
   text.gsub(/(https?:\/\/)?(www.)?soundcloud\.com\/\S*/) do |match|
     new_uri = match.to_s
-    new_uri = (new_uri =~ /^https?\:\/\/.*/) ? URI(new_uri) : URI("http://#{new_uri}")
+    new_uri = (new_uri =~ /(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/) ? URI(new_uri) : URI("http://#{new_uri}")
     new_uri.normalize!
     width = options[:width]
     height = options[:height]
