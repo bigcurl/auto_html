278eacb487db19799afe326300f2f437d0bd8967
Sam <sam@bigcurl.de>
2014-11-27 23:37:19 +0100

    added uri parser and ssl soundcloud links

--- lib/auto_html/filters/soundcloud.rb
+++ lib/auto_html/filters/soundcloud.rb
@@ -4,9 +4,10 @@ require 'uri'
 # set these options and default values
 # :width => '100%', :height => 166, :auto_play => false, :theme_color => '00FF00', :color => '915f33', :show_comments => false
 AutoHtml.add_filter(:soundcloud).with(:width => '100%', :height => 166, :auto_play => false, :theme_color => '00FF00', :color => '915f33', :show_comments => false, :show_artwork => false) do |text, options|
-  text.gsub(/(https?:\/\/)?(www.)?soundcloud\.com\/[^\s`!()\[\]{};:'".,<>?«»“”‘’]*/) do |match|
+  text.gsub(/(https?:\/\/)?(www.)?soundcloud\.com\/\S*/) do |match|
     new_uri = match.to_s
-    new_uri = (new_uri =~ /(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/) ? URI(new_uri) : URI("http://#{new_uri}")
+    new_uri = URI::Parser.new.parse new_uri
+    new_uri = (new_uri =~ /^https?\:\/\/.*/) ? URI(new_uri) : URI("https://#{new_uri}")
     new_uri.normalize!
     width = options[:width]
     height = options[:height]
@@ -15,6 +16,6 @@ AutoHtml.add_filter(:soundcloud).with(:width => '100%', :height => 166, :auto_pl
     color = options[:color]
     show_artwork = options[:show_artwork]
     show_comments = options[:show_comments]
-    %{<iframe width="#{width}" height="#{height}" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=#{new_uri}&show_artwork=#{show_artwork}&show_comments=#{show_comments}&auto_play=#{auto_play}&color=#{color}&theme_color=#{theme_color}"></iframe>}
+    %{<iframe width="#{width}" height="#{height}" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=#{new_uri}&show_artwork=#{show_artwork}&show_comments=#{show_comments}&auto_play=#{auto_play}&color=#{color}&theme_color=#{theme_color}"></iframe>}
   end
 end
