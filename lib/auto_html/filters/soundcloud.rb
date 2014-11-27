abc4d411c7da76fadebab49f6ed36ecd67cddf3d
Sam <sam@bigcurl.de>
2014-11-28 00:10:13 +0100

    it is an object ;-)

--- lib/auto_html/filters/soundcloud.rb
+++ lib/auto_html/filters/soundcloud.rb
@@ -6,8 +6,8 @@ require 'uri'
 AutoHtml.add_filter(:soundcloud).with(:width => '100%', :height => 166, :auto_play => false, :theme_color => '00FF00', :color => '915f33', :show_comments => false, :show_artwork => false) do |text, options|
   text.gsub(/(https?:\/\/)?(www.)?soundcloud\.com\/\S*/) do |match|
     new_uri = match.to_s
-    new_uri.match(/(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/)
-    new_uri = ($1 =~ /^https?\:\/\/.*/) ? URI($1) : URI("https://#{$1}")
+    clean_url = new_uri.match(/(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/)
+    new_uri = (clean_url[0] =~ /^https?\:\/\/.*/) ? URI(clean_url[0]) : URI("https://#{clean_url[0]}")
     new_uri.normalize!
     width = options[:width]
     height = options[:height]
