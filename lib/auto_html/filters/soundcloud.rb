63b87e1b9b098d4b14ae952280dcacb2731f4457
Sam <sam@bigcurl.de>
2014-11-27 23:30:03 +0100

    new regex should be here

--- lib/auto_html/filters/soundcloud.rb
+++ lib/auto_html/filters/soundcloud.rb
@@ -4,7 +4,7 @@ require 'uri'
 # set these options and default values
 # :width => '100%', :height => 166, :auto_play => false, :theme_color => '00FF00', :color => '915f33', :show_comments => false
 AutoHtml.add_filter(:soundcloud).with(:width => '100%', :height => 166, :auto_play => false, :theme_color => '00FF00', :color => '915f33', :show_comments => false, :show_artwork => false) do |text, options|
-  text.gsub(/(https?:\/\/)?(www.)?soundcloud\.com\/\S*/) do |match|
+  text.gsub(/(https?:\/\/)?(www.)?soundcloud\.com\/[^\s`!()\[\]{};:'".,<>?«»“”‘’]*/) do |match|
     new_uri = match.to_s
     new_uri = (new_uri =~ /(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/) ? URI(new_uri) : URI("http://#{new_uri}")
     new_uri.normalize!
