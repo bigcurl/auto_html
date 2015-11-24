c7a9ea261062a21d93a601ca9e6d51ff13e4157e
Sam <sam@bigcurl.de>
2015-11-25 00:50:03 +0100

    missed this

--- lib/auto_html/filters/sanitize.rb
+++ lib/auto_html/filters/sanitize.rb
@@ -2,8 +2,6 @@ require 'action_controller'
 require 'cgi'
 require 'rails-html-sanitizer'
 
-=======
->>>>>>> master
 AutoHtml.add_filter(:sanitize).with({}) do |text, options|
   require 'action_controller'
   require 'cgi'
